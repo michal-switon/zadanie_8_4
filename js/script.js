24e02ffaf5022da9d63e706f802630a94a0c80ee
Michał Świtoń <michal.switon@op.pl>
2017-12-26 19:00:21 +0100

    add files to commit and add comments in script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var name = prompt('Enter your name'); 
+alert('Hello, ' + name); //This part takes what you've written and displays it
+console.log('Hello, ' + name); //This part displays result in console
+/*
+This comment can take more than
+only 1 line of code!
+*/
